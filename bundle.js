d2491d5d11739c30af28586a9b9cb9d134551770
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-16 18:51:44 -0500

    finished

--- bundle.js
+++ bundle.js
@@ -1,32 +1,81 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var $ = require('jquery');
+var _ = require('underscore');
 var PictureCollection = require('./pictureCollection');
 var PictureModel = require('./pictureModel');
 
 
-module.exports = {
+module.exports = grahamPage = {
   init: function () {
-    this.styling();
-    this.events();
+    grahamPage.styling();
+    grahamPage.events();
   },
+  pictureCollection: new PictureCollection(),
   styling: function() {
-    console.log("yay!");
+    grahamPage.loadPictures();
+
   },
   events: function() {
     $("#imageUploadSubmit").on("click",function (e) {
       e.preventDefault();
-      var imageCaption = $("#imageCaption").val();
-      var imageURL = $("#imageURL").val();
-      var picture = {caption: imageCaption, url: imageURL};
-      var newPicModel = new PictureModel();
-      newPicModel.save();
-      
+      var newPicModel = new PictureModel({caption: $("#imageCaption").val(), url: $("#imageURL").val()});
+      newPicModel.save().then(function () {
+        $(".pics").prepend(_.template($("#picTmpl").html())(newPicModel));
+      })
+      $("#imageCaption").val("");
+      $("#imageURL").val("");
+    });
+
+    $(".pics").on("click",".fa-heart-o",function (e) {
+      e.preventDefault();
+      var id = $(this).attr("id");
+      $(this).toggleClass("fa-heart-o");
+      $(this).toggleClass("fa-heart");
+      var likedPic = grahamPage.pictureCollection.get(id);
+      var likes = likedPic.get('likes')+1;
+      likedPic.set('likes',likes);
+      likedPic.save();
+      $($(this).parent().siblings("span")[0]).html("  "+likes+" likes");
+      // grahamPage.loadPictures();
+    });
+    $(".pics").on("click",".fa-heart",function (e) {
+      e.preventDefault();
+      var id = $(this).attr("id");
+      $(this).toggleClass("fa-heart-o");
+      $(this).toggleClass("fa-heart");
+      var likedPic = grahamPage.pictureCollection.get(id);
+      var likes = likedPic.get('likes')-1;
+      likedPic.set('likes',likes);
+      likedPic.save();
+      $($(this).parent().siblings("span")[0]).html("  "+likes+" likes");
+      // grahamPage.loadPictures();
+    });
+    $(".pics").on("click",".delete",function (e) {
+      e.preventDefault();
+      var id = $(this).attr("id");
+      id = id.substring(0,id.length-6)
+      var deletedPic = grahamPage.pictureCollection.get(id);
+      deletedPic.destroy();
+      $(this).parents(".row")[0].remove();
+      grahamPage.loadPictures();
     })
   },
+  loadPictures: function (collection) {
+    $(".pics").html("");
+    grahamPage.pictureCollection.fetch().then(function () {
+      _.each(grahamPage.pictureCollection.models, function (picture) {
+        grahamPage.loadTemplate("#picTmpl", picture, ".pics");
+      })
+    })
+  },
+  loadTemplate: function(template, object, element){
+    var temp = _.template($(template).html());
+    $(element).append(temp(object));
+  }
 
 }
 
-},{"./pictureCollection":6,"./pictureModel":7,"jquery":4}],2:[function(require,module,exports){
+},{"./pictureCollection":6,"./pictureModel":7,"jquery":4,"underscore":5}],2:[function(require,module,exports){
 var $ = require('jquery');
 var PictureCollection = require('./pictureCollection');
 var PictureModel = require('./pictureModel');
--- graham.js
+++ graham.js
@@ -1,26 +1,75 @@
 var $ = require('jquery');
+var _ = require('underscore');
 var PictureCollection = require('./pictureCollection');
 var PictureModel = require('./pictureModel');
 
 
-module.exports = {
+module.exports = grahamPage = {
   init: function () {
-    this.styling();
-    this.events();
+    grahamPage.styling();
+    grahamPage.events();
   },
+  pictureCollection: new PictureCollection(),
   styling: function() {
-    console.log("yay!");
+    grahamPage.loadPictures();
+
   },
   events: function() {
     $("#imageUploadSubmit").on("click",function (e) {
       e.preventDefault();
-      var imageCaption = $("#imageCaption").val();
-      var imageURL = $("#imageURL").val();
-      var picture = {caption: imageCaption, url: imageURL};
-      var newPicModel = new PictureModel();
-      newPicModel.save();
-      
+      var newPicModel = new PictureModel({caption: $("#imageCaption").val(), url: $("#imageURL").val()});
+      newPicModel.save().then(function () {
+        $(".pics").prepend(_.template($("#picTmpl").html())(newPicModel));
+      })
+      $("#imageCaption").val("");
+      $("#imageURL").val("");
+    });
+
+    $(".pics").on("click",".fa-heart-o",function (e) {
+      e.preventDefault();
+      var id = $(this).attr("id");
+      $(this).toggleClass("fa-heart-o");
+      $(this).toggleClass("fa-heart");
+      var likedPic = grahamPage.pictureCollection.get(id);
+      var likes = likedPic.get('likes')+1;
+      likedPic.set('likes',likes);
+      likedPic.save();
+      $($(this).parent().siblings("span")[0]).html("  "+likes+" likes");
+      // grahamPage.loadPictures();
+    });
+    $(".pics").on("click",".fa-heart",function (e) {
+      e.preventDefault();
+      var id = $(this).attr("id");
+      $(this).toggleClass("fa-heart-o");
+      $(this).toggleClass("fa-heart");
+      var likedPic = grahamPage.pictureCollection.get(id);
+      var likes = likedPic.get('likes')-1;
+      likedPic.set('likes',likes);
+      likedPic.save();
+      $($(this).parent().siblings("span")[0]).html("  "+likes+" likes");
+      // grahamPage.loadPictures();
+    });
+    $(".pics").on("click",".delete",function (e) {
+      e.preventDefault();
+      var id = $(this).attr("id");
+      id = id.substring(0,id.length-6)
+      var deletedPic = grahamPage.pictureCollection.get(id);
+      deletedPic.destroy();
+      $(this).parents(".row")[0].remove();
+      grahamPage.loadPictures();
     })
   },
+  loadPictures: function (collection) {
+    $(".pics").html("");
+    grahamPage.pictureCollection.fetch().then(function () {
+      _.each(grahamPage.pictureCollection.models, function (picture) {
+        grahamPage.loadTemplate("#picTmpl", picture, ".pics");
+      })
+    })
+  },
+  loadTemplate: function(template, object, element){
+    var temp = _.template($(template).html());
+    $(element).append(temp(object));
+  }
 
 }
